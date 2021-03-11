ea516f98869fc78325e8821e72328068fd958b3e
Vinicius Paes <viniciusjosepaes@hotmail.com>
2021-03-10 21:39:21 -0300

    Adjust on functions docstring and param names
    Adjusting the functions description to match the current functionalities.

--- automato.py
+++ automato.py
@@ -1,14 +1,14 @@
 class Automata:
-    def __init__(self, alphabet, transition, initial, final):
+    def __init__(self, alphabet, transistion, initial, final):
         """
-        A automata able to recognize languages given a alphabet and a transition table.
+        A automata able to recognize languages given a alphabet and a transistion table.
         :param alphabet: Receives the alphabet in the format [n1, n2, ..., nn] 
-        :param transition: Receives the transistion table as a dictionary in the format
+        :param transistion: Receives the transistion table as a dictionary in the format
         {'q1': {'0': 'q1', '1': 'q2'}, 'q2': {'0': 'q1', '1': 'q3'}, 'q3': {'0': 'q3', '1': 'q3'}}
-        :param initial: The initial state must be present inside the transition dictionary
-        :param final: The final state must be present inside the transition dictionary
+        :param initial: The initial state must be present inside the transistion dictionary
+        :param final: The final state must be present inside the transistion dictionary
         """
-        self.transition_fuction = transition
+        self.transistion_fuction = transistion
         self.alphabet = alphabet
         self.current_state = None
         self.initial_state = initial
@@ -18,7 +18,7 @@ class Automata:
         """
         Function responsible for validate a given language
         :param language: The languagem in string format that must be validated
-        :return: 
+        :return: True if language is valid, False if not
         """
         self.current_state = self.initial_state
         for element in language:
@@ -26,7 +26,7 @@ class Automata:
                 self.current_state = None
                 return "Language contains characters not included in this automato alphabet"
             try:
-                self.current_state = self.transition_fuction[str(self.current_state)][element]
+                self.current_state = self.transistion_fuction[str(self.current_state)][element]
                 print("Actual state {}".format(self.current_state))
             except KeyError:
                 self.current_state = None
@@ -38,7 +38,14 @@ class Automata:
         return False #language didn't reach final state, it is not valid
 
     def upgrade_machine(self, alphabet, transistion, intial, final):
+        """
+        Upgrades the automato values transforming him into a new automato
+        :param alphabet: the new recognized alphabet 
+        :param transistion: the new recognized transistion table
+        :param initial: the new initial state
+        :param final: the new final state
+        """
         self.alphabet = alphabet
-        self.transition_fuction = transistion
+        self.transistion_fuction = transistion
         self.initial_state = intial
         self.final_state = final
